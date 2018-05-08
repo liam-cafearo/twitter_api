57d7d16d2a5c9ad7ebf367bcce5aaa0847d152e9
Liam-Cafearo <lcafearo@gmail.com>
2018-05-08 21:39:23 +0100

    Added second city to trending data

--- twitter_intro.py
+++ twitter_intro.py
@@ -13,7 +13,10 @@ auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 DUB_WOE_ID = 560743
+LON_WOE_ID = 44418
 
 dub_trends = api.trends_place(DUB_WOE_ID)
+lon_trends = api.trends_place(LON_WOE_ID)
 
 print json.dumps(dub_trends, indent=1)
+print json.dumps(lon_trends, indent=1)
