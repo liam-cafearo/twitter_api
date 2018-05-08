9c8aa28eb9c8785a62371e5f377a92515d4dfc14
Liam-Cafearo <lcafearo@gmail.com>
2018-05-08 20:35:21 +0100

    Added tweepy api and what's trending data

--- twitter_intro.py
+++ twitter_intro.py
@@ -10,5 +10,10 @@ OAUTH_TOKEN_SECRET = 'bfyjnwr64SWeh4U49fH9CIZC00BgMK4Yvz5DxbhkVQOwt'
 
 auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
 api = tweepy.API(auth)
+
+DUB_WOE_ID = 560743
+
+dub_trends = api.trends_place(DUB_WOE_ID)
+
+print json.dumps(dub_trends, indent=1)
