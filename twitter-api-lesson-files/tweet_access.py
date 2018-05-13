e735a8d7973ddff2e3f0faf0d3a8954d0bd5a797
Liam-Cafearo <lcafearo@gmail.com>
2018-05-13 13:50:40 +0100

    Added status objects to make information clearer

--- twitter-api-lesson-files/tweet_access.py
+++ twitter-api-lesson-files/tweet_access.py
@@ -14,6 +14,12 @@ query = 'Dublin'
 
 # Get all status
 results = [status for status in tweepy.Cursor(api.search, q=query).items(count)]
+print json.dumps(results[0]._json, indent=4)
 
-for result in results:
-    print json.dumps(result._json, indent=2)
+for status in results:
+    print status.text.encode('utf-8')
+    print status.user.id
+    print status.user.screen_name
+    print status.user.profile_image_url_https
+    print status.user.followers_count
+    print status.place
