35f7196189db533c8c6b3f22dc062bd4b573deeb
Liam-Cafearo <lcafearo@gmail.com>
2018-05-15 20:31:58 +0100

    deleted files

---
+++ twitter-api-lesson-files/credentials.py
@@ -0,0 +1,5 @@
+# Replace these values with our own twitter app settings
+CONSUMER_KEY = 'AK0Rw5s8ldBk9gmgqkX088XWD'
+CONSUMER_SECRET = 'YnHl755jyuDwnDLcpHG3ps1cnggD13Xeq3ZwFtp645jjKUjlz2'
+OAUTH_TOKEN = '483463582-x6m4lHVpZxYpQFjQIp15nxIC25uK6L5uIziw1cs4'
+OAUTH_TOKEN_SECRET = 'bfyjnwr64SWeh4U49fH9CIZC00BgMK4Yvz5DxbhkVQOwt'
