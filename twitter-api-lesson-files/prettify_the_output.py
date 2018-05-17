27a9c233b8203789ab6a22ccdcf2120ac4873519
Liam-Cafearo <lcafearo@gmail.com>
2018-05-17 20:27:46 +0100

    added lexical diversity code

--- twitter-api-lesson-files/prettify_the_output.py
+++ twitter-api-lesson-files/prettify_the_output.py
@@ -40,3 +40,31 @@ for label, data in (('Text', status_texts),
         [ table.add_row(entry) for entry in counter.most_common()[:10] ]
         table.align[label], table.align['Count'] = 'l', 'r' # align the columns
         print table
+
+# Lexical Diversity Lesson Code
+
+# Take a list of items passed as a parameter to the 
+# get_lexical_diversity function
+def get_lexical_diversity(items):
+        # Change the items to set to ensure all items are unique
+        # get the length of the set
+        # divide the set length by the length of the items list
+        # multiply by 1.0 to avoide rounding off to the nearest integer
+        return 1.0*len(set(items))/len(items)
+
+# Take a list of tweets passed as a parameter to the
+# get_average_words function
+def get_average_words(tweets):
+        # split each tweet into words
+        # get the number of resulting words
+        # sum each result set
+        total_words = sum([ len(tweet.split()) for tweet in tweets ])
+        # Divide the sum by the number of tweets to get the average
+        # multiply by 1.0 to avoid rounding off to the nearest integer
+        return 1.0*total_words/len(tweets)
+
+# Invoke the functions by passing in our lists gathered earlier
+print "Average words: %s" % get_average_words(status_texts)
+print "Word Diversity: %s" % get_lexical_diversity(words)
+print "Screen Name Diversity: %s" % get_lexical_diversity(screen_names)
+print "HashTag Diversity: %s" % get_lexical_diversity(hashtags)
